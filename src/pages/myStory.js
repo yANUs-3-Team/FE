9f99cae23e2d8acafd6c1e49b968a2da94719eb0
Radian06 <aksen05aksen@gmail.com>
2025-06-18 22:13:03 +0900

    Start my story page

--- src/pages/myStory.js
+++ src/pages/myStory.js
@@ -1,11 +1,20 @@
 import Footer from "../component/footer";
 import "../component/Css/myStory.css";
+import halfSide3 from "../images/character_halfSide3.png";
 
 function MyStory() {
     return (
         <>
             <div className="myStory_page">
-                나의 동화책 화면 테스트
+                <div className="decoCircle_box">
+                    <div className="circleBlue1"></div>
+                    <div className="circleBlue2"></div>
+                    <div className="circleBlue3"></div>
+                </div>
+                <div className="myStory_title">나의 동화책</div>
+                <div className="storyIcon_box">
+                    <img src={halfSide3} alt="" className="MScharIMG"></img>
+                </div>
             </div>
             <Footer />
         </>
