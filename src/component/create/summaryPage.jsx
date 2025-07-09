873891a36b3a51cbe6c2a819108138aaa363ad88
Radian06 <aksen05aksen@gmail.com>
2025-07-09 11:26:15 +0900

    Modified radio setting

--- src/component/create/summaryPage.jsx
+++ src/component/create/summaryPage.jsx
@@ -1,6 +1,17 @@
 import "../../component/Css/summaryPage.css";
 
-function SummaryPage({ name, personality, characteristics, location, era, genre, visibility, setPageIndex, navigate }) {
+function SummaryPage({
+  name,
+  personality,
+  characteristics,
+  location,
+  era,
+  genre,
+  visibility,
+  setVisibility, 
+  setPageIndex,
+  navigate,
+}) {
   return (
     <div className="select_box2">
       <div className="select_container2">
@@ -16,7 +27,9 @@ function SummaryPage({ name, personality, characteristics, location, era, genre,
       <div className="select_container2">
         <div className="selected_group">
           <div className="selected_label">주인공의 특징</div>
-          <div className="selected_value2">{characteristics || "선택되지 않음"}</div>
+          <div className="selected_value2">
+            {characteristics || "선택되지 않음"}
+          </div>
         </div>
       </div>
       <div className="select_container2">
@@ -40,7 +53,7 @@ function SummaryPage({ name, personality, characteristics, location, era, genre,
             name="visibility"
             value="private"
             checked={visibility === "private"}
-            onChange={() => {}}
+            onChange={() => setVisibility("private")}
           />
           나만 보기
         </label>
@@ -50,16 +63,22 @@ function SummaryPage({ name, personality, characteristics, location, era, genre,
             name="visibility"
             value="public"
             checked={visibility === "public"}
-            onChange={() => {}}
+            onChange={() => setVisibility("public")}
           />
           전체 공개
         </label>
       </div>
       <div className="pagination_box">
-        <div className="pagination_button left_button" onClick={() => setPageIndex(0)}>
+        <div
+          className="pagination_button left_button"
+          onClick={() => setPageIndex(0)}
+        >
           설정 바꾸기
         </div>
-        <div className="pagination_button right_button" onClick={() => navigate("/loading")}>
+        <div
+          className="pagination_button right_button"
+          onClick={() => navigate("/loading")}
+        >
           동화 만들기
         </div>
       </div>
