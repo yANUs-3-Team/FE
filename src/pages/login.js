56866266bb7645e57010a3313a7127525568b297
Radian06 <aksen05aksen@gmail.com>
2025-07-17 10:14:56 +0900

    Changed pointer shape

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,11 @@
 import "../component/Css/login.css";
 import mainLogo from "../images/mainLogo.png";
 import googleLogo from "../images/google_icon.png";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
   return (
     <div className="login_page">
       <div className="login_circle_back"></div>
@@ -18,13 +21,13 @@ function Login() {
         <div className="social_login_box">
           <div className="social_login social_kakao">
             <div className="social_icon_box">
-                <div className="kakao_icon"></div>
+              <div className="kakao_icon"></div>
             </div>
             Kakao 계정으로 로그인
           </div>
           <div className="social_login social_google">
             <div className="social_icon_box">
-                <img src={googleLogo} alt="" className="google_icon"></img>
+              <img src={googleLogo} alt="" className="google_icon"></img>
             </div>
             Google 계정으로 로그인
           </div>
@@ -34,7 +37,9 @@ function Login() {
 
         <div className="login_find">아이디/비밀번호 찾기</div>
 
-        <div className="login_toJoin">회원가입 하기</div>
+        <div className="login_toJoin" onClick={() => navigate("/join")}>
+          회원가입 하기
+        </div>
       </div>
     </div>
   );
