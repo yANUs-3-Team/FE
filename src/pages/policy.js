d564056ffaf0c36237d1bea3586754bc7b0cbd76
Radian06 <aksen05aksen@gmail.com>
2025-07-17 13:58:58 +0900

    Add bookmark function

--- src/pages/policy.js
+++ src/pages/policy.js
@@ -3,30 +3,46 @@ import { useEffect, useState } from "react";
 
 function Policy() {
   const [privacyText, setPrivacyText] = useState("");
-  /*const [termsText, setTermsText] = useState("");*/
+  const [termsText, setTermsText] = useState("");
+  const [selectedPolicy, setSelectedPolicy] = useState("privacy"); // 초기값은 개인정보처리방침
 
   useEffect(() => {
     fetch("/privacy.txt")
       .then((res) => res.text())
       .then((text) => setPrivacyText(text));
 
-      /*fetch("/terms.txt")
+    fetch("/terms.txt")
       .then((res) => res.text())
-      .then((text) => setTermsText(text));*/
+      .then((text) => setTermsText(text));
   }, []);
 
   return (
     <div className="policy_page">
       <div className="policy_container">
-        <div className="policy_title">[ 개인정보 수집 · 이용 동의서 ]</div>
-          <div className="policy_paragraphs_box">{privacyText}</div>
+        <div className="policy_title">
+          {selectedPolicy === "privacy"
+            ? "[ 개인정보 수집 · 이용 동의서 ]"
+            : "[ 이용약관 ]"}
+        </div>
 
-          {/*<div className="policy_paragraphs_box">{termsText}</div>*/}
+        <div className="policy_paragraphs_box">
+          {selectedPolicy === "privacy" ? privacyText : termsText}
+        </div>
       </div>
-      
+
       <div className="policy_bookmark_box">
-        <div className="policy_bookmark">개인정보처리방침</div>
-        <div className="policy_bookmark">이용약관</div>
+        <div
+          className={`policy_bookmark ${selectedPolicy === "privacy" ? "active" : ""}`}
+          onClick={() => setSelectedPolicy("privacy")}
+        >
+          개인정보처리방침
+        </div>
+        <div
+          className={`policy_bookmark ${selectedPolicy === "terms" ? "active" : ""}`}
+          onClick={() => setSelectedPolicy("terms")}
+        >
+          이용약관
+        </div>
       </div>
     </div>
   );
