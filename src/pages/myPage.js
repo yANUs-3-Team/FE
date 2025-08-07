86a9ef9effbd6885c518e9d0968d8ad1bae9cf15
Radian06 <aksen05aksen@gmail.com>
2025-08-07 13:07:33 +0900

    ⚡️ :: 마이페이지 UI퍼블리싱

--- src/pages/myPage.js
+++ src/pages/myPage.js
@@ -1,10 +1,43 @@
 import Footer from "../component/footer";
 import "../component/Css/myPage.css";
+import profile from "../images/default_profile.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
 function MyPage() {
   return (
     <>
-      <div className="myPage_page">마이페이지</div>
+      <div className="myPage_page">
+        <div className="tag_container">
+          <div className="tag_box">
+            <img src={profile} alt="" className="tag_profile" />
+            <div className="tag_contour"></div>
+            <div className="tag_detailBox">
+              <div className="tag_titleBox">
+                <div className="tag_idBox">
+                  <div className="tag_subId">동화작가</div>
+                  <div className="tag_id">사용자</div>
+                </div>
+                <div className="tag_edit"><FontAwesomeIcon icon={faPenToSquare} /></div>
+              </div>
+              <div className="tag_subBox">
+                <div className="tag_leftBox">
+                  <div className="tag_info">이름: ○○○</div>
+                  <div className="tag_info">비밀번호: ********</div>
+                  <div className="tag_info">이메일: asdfghjkl@gmail.com</div>
+                  <div className="tag_info">생성한 동화책 수: n 권</div>
+                </div>
+                <div className="tag_rightBox">
+                  <div className="tag_info">가입 날짜: 20XX.XX.XX</div>
+                  <div className="tag_info">생년월일: 20XX.XX.XX</div>
+                  <div className="tag_info">간편 로그인: </div>
+                  <div className="tag_info">공개 동화책 수: n 권</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
