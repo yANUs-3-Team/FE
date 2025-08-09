c968b6290b6d9abad042e8e4931749e6dce325f6
Radian06 <aksen05aksen@gmail.com>
2025-08-10 02:13:06 +0900

    ⚡️ :: 커뮤니티 퍼블리싱 중

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,5 +1,7 @@
 import Footer from "../component/footer";
 import "../component/Css/community.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 function Community() {
   return (
@@ -15,21 +17,82 @@ function Community() {
         </div>
 
         <div className="commu_wholeBox">
-          <div className="commu_myContainer"></div>
+          <div className="commu_myContainer">
+            <div className="commu_myButton">📝 내가 쓴 글</div>
+            <div className="commu_myContour"></div>
+            <div className="commu_myButton">💬 댓글 단 글</div>
+            <div className="commu_myContour"></div>
+            <div className="commu_myButton">❤️ 좋아요한 글</div>
+          </div>
+
           <div className="commu_mainBox">
-            <div className="commu_topContainer"></div>
-            <div className="commu_writeContainer"></div>
-            <div className="commu_listContainer">
-              <div className="commu_postContainer"></div>
-              <div className="commu_postContainer"></div>
-              <div className="commu_postContainer"></div>
-              <div className="commu_postContainer"></div>
-              <div className="commu_postContainer"></div>
-              <div className="commu_postContainer"></div>
+            <div className="commu_topContainer">
+              <div className="commu_topText">검색 결과</div>
+              <select className="commu_topSelect">
+                <option value="title">제목</option>
+                <option value="author">작가</option>
+                <option value="date">날짜</option>
+              </select>
+              <input className="commu_topInput" />
+              <div className="commu_topButton">검색</div>
             </div>
+
+            <div className="commu_writeContainer">
+              <p>새 글을 작성해 주세요!</p>
+              <FontAwesomeIcon icon={faPen} />
+            </div>
+
+            <div className="commu_listContainer">
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+              <div className="commu_postContour"></div>
+              <div className="commu_postContainer">
+                <div className="commu_postTitle">제목</div>
+                <div className="commu_postDate">mm분 전</div>
+              </div>
+            </div>
+
             <div className="commu_bottomContainer"></div>
           </div>
-          <div className="commu_bestContainer"></div>
+
+          <div className="commu_bestContainer">
+            <div className="commu_bestTitle">🔥 인기 게시물</div>
+            <div className="commu_bestContour"></div>
+            <div className="commu_bestPostBox">
+              <div className="commu_bestPost">인기 게시글 1</div>
+              <div className="commu_bestContour"></div>
+              <div className="commu_bestPost">인기 게시글 2</div>
+              <div className="commu_bestContour"></div>
+              <div className="commu_bestPost">인기 게시글 3</div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
