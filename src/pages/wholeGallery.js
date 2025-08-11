1d2b640de1ebd1badf1c827ff94efbf788841485
Radian06 <aksen05aksen@gmail.com>
2025-08-11 21:27:41 +0900

    ⚡️ :: 갤러리 퍼블리싱 중

--- src/pages/wholeGallery.js
+++ src/pages/wholeGallery.js
@@ -1,11 +1,18 @@
 import "../component/Css/wholeGallery.css";
 import Footer from "../component/footer";
+import { useNavigate } from "react-router-dom";
 
 function WholeGallery() {
+    const navigate = useNavigate();
     return (
         <>
             <div className="wholeGallery_page">
-                WG test
+                <div className="WG_topBox">
+                    <div className="goTo_myGallery" onClick={() => navigate("/my-gallery")}>나의 갤러리</div>
+                    <div className="goTo_openGallery" onClick={() => navigate("/open-gallery")}>공개 갤러리</div>
+                </div>
+
+                <div className="WG_bottomBox"></div>
             </div>
             <Footer />
         </>
