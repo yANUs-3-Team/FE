d6104542307083c45d2c4e5abb9c6dc3a4b841b4
Radian06 <aksen05aksen@gmail.com>
2025-08-19 14:00:31 +0900

    ⚡️ ::직접 입력 -> 선택지 4로 임시 변경

--- src/pages/interactiveStory.js
+++ src/pages/interactiveStory.js
@@ -10,11 +10,11 @@ function InteractiveStory() {
   const rawPages = [
     {
       image: Illust, // ✅ import된 PNG -> 문자열 URL
-      text:
-        "윈터는 화려한 금색 단발을 휘날리며 성 안에서 즐겁게 뛰어놀고 있었다. 그러던 중, 창문 너머로 낯선 빛이 들어오는 것을 보았다.",
+      text: "윈터는 화려한 금색 단발을 휘날리며 성 안에서 즐겁게 뛰어놀고 있었다. 그러던 중, 창문 너머로 낯선 빛이 들어오는 것을 보았다.",
       select1: "방 안에서 나비에게 조심스럽게 인사를 건넨다.",
       select2: "방 안을 둘러보면서 나비와의 특별한 만남을 상상한다.",
       select3: "나비를 쫓아다니며 장난을 치고 싶어진다.",
+      select4: "나비 쪽을 향해 손을 뻗는다.",
     },
     { image: "🌄 삽화 2", text: "📖 이야기 2" }, // 이건 텍스트(이모지)로 렌더
     { image: "🌄 삽화 3", text: "📖 이야기 3" },
@@ -45,21 +45,39 @@ function InteractiveStory() {
       <div className="IS_text_box">{page.text}</div>
       <div className="IS_select_box">
         {page.select1 && (
-          <button className="IS_select" onClick={() => handleChoiceClick(page.select1)}>
+          <button
+            className="IS_select"
+            onClick={() => handleChoiceClick(page.select1)}
+          >
             {page.select1}
           </button>
         )}
         {page.select2 && (
-          <button className="IS_select" onClick={() => handleChoiceClick(page.select2)}>
+          <button
+            className="IS_select"
+            onClick={() => handleChoiceClick(page.select2)}
+          >
             {page.select2}
           </button>
         )}
         {page.select3 && (
-          <button className="IS_select" onClick={() => handleChoiceClick(page.select3)}>
+          <button
+            className="IS_select"
+            onClick={() => handleChoiceClick(page.select3)}
+          >
             {page.select3}
           </button>
         )}
-        <input
+        {page.select4 && (
+          <button
+            className="IS_select"
+            onClick={() => handleChoiceClick(page.select4)}
+          >
+            {page.select4}
+          </button>
+        )}
+        {/* 보류 */}
+        {/**<input
           type="text"
           className="IS_select_input"
           placeholder="당신이 직접 입력해 보세요!"
@@ -68,7 +86,7 @@ function InteractiveStory() {
               handleChoiceClick(e.currentTarget.value.trim());
             }
           }}
-        />
+        /> */}
       </div>
     </div>,
   ]);
